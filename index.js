fa1bac29a86f121373d0432ab559d4ccb68bacab
Samrat Ahmmed <samratahammed29@gmail.com>
2023-11-18 17:23:55 +0600

    add user collection and get api impliment

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.Db_USER}:${process.env.DB_PASS}@cluster0.9zu3h4f.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,7 @@ async function run() {
     const menuCollection = client.db("bistroBD").collection("menu");
     const reviewsCollection = client.db("bistroBD").collection("reviews");
     const cartsCollection = client.db("bistroBD").collection("carts");
+    const userCollection = client.db("bistroBD").collection("user");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -52,6 +53,32 @@ async function run() {
     app.post("/carts", async (req, res) => {
       const cart = req.body;
       const result = await cartsCollection.insertOne(cart);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // user ..............
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result);
     });
 
