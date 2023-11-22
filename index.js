c5f23e8ca6c04fb474d706bcceea9fd69d5e6ded
Samrat Ahmmed <samratahammed29@gmail.com>
2023-11-22 15:32:47 +0600

    add stripe

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KYE);
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 // middleware
@@ -44,7 +45,7 @@ async function run() {
 
     // middleware............
     const verifyToken = (req, res, next) => {
-      console.log("inside verify token", req.headers.authorization);
+      // console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "unAuthorized access" });
       }
@@ -76,6 +77,46 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+
+    app.post("/menu", verifyToken, adminVerify, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/menu/:id", verifyToken, adminVerify, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/menu/:id", async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: id };
+      const updateDoc = {
+        $set: {
+          name: item.name,
+          price: item.price,
+          recipe: item.recipe,
+          category: item.category,
+          image: item.image,
+        },
+      };
+      const result = await menuCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // revire...................
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
@@ -173,6 +214,23 @@ async function run() {
         res.send(result);
       }
     );
+
+    // payment.............
+
+    app.post("/create-payment-intern", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, "amount inside then payment");
+      const paymentIntern = await stripe.paymentInterns.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_type: ["card"],
+      });
+
+      res.send({
+        clintSecret: paymentIntern.client_secret,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
